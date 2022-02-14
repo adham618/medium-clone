4b3b93d87b24546e7c036608fe484d33e745862f
adham618 <adhamtarek291@gmail.com>
2022-02-14 13:31:00 +0200

    complete Medium section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
+import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
 
 export default function HomePage() {
   return (
-    <Layout>
-      {/* <Seo templateTitle='Home' /> */}
+    <div className='max-w-7xl mx-auto'>
+      <Seo templateTitle='Home' />
       <Seo />
-
+      <section
+        className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0"
+      >
+        <div className="px-10 space-y-5">
+          <h1 className='text-6xl max-w-xl font-serif'>
+            <span className='underline decoration-black decoration-4'>
+              Medium
+            </span>{" "}
+            is a place to write, read and connect
+          </h1>
+          <h2>
+            It&apos;s easy and free to post your thinking on any topic and connect with milions of readers.
+          </h2>
+        </div>
+        <NextImage layout='intrinsic' className='hidden pr-11 md:inline-flex h-32 lg:h-full' src="/svg/medium-1.svg" width={300} height={400} alt="logo" draggable="false" />
+      </section>
       <main>
-        <section className='bg-white'></section>
       </main>
-    </Layout>
+    </div>
   );
 }
